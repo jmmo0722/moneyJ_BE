c55fc62c0b8a17039e8cebf9b2282a206c3099ac
mojiri00 <moguricamera@gmail.com>
2025-08-24 01:06:53 +0900

    feat: 스프링 부트 초기 설정

---
+++ src/main/java/com/project/moneyj/MoneyjApplication.java
@@ -0,0 +1,13 @@
+package com.project.moneyj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/moneyj/MoneyjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.moneyj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
