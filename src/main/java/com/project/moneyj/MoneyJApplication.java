f4186a075530529df7de123d00d883b58f8f8c1c
mojiri00 <moguricamera@gmail.com>
2025-08-24 12:55:53 +0900

    refactor: moneyj -> moneyJ 로 명칭 변경

--- src/main/java/com/project/moneyj/MoneyJApplication.java
+++ src/main/java/com/project/moneyj/MoneyJApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MoneyjApplication {
+public class MoneyJApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MoneyjApplication.class, args);
+		SpringApplication.run(MoneyJApplication.class, args);
 	}
 
 }
