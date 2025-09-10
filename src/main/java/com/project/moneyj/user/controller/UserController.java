6d04e42fab02d52720f8bb09e710e0f547d94a0a
hyunseo <wndlthsk@naver.com>
2025-09-10 16:16:43 +0900

    fix: 이메일 확인 post로 변경

--- src/main/java/com/project/moneyj/user/controller/UserController.java
+++ src/main/java/com/project/moneyj/user/controller/UserController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +28,7 @@ public class UserController {
         return ResponseEntity.ok(userResponseDTO);
     }
 
-    @GetMapping("/check")
+    @PostMapping("/check")
     public ResponseEntity<List<UserCheckResponseDTO>> checkUserByEmail(
         @RequestBody UserCheckRequestDTO request
     ) {
